ef854566836a8d9e4a81fbf5624a2c96f613a12d
Dmitry Dementiev <info@dementiev.net>
2022-06-23 16:29:19 +0300

    Изменена сборка PHP

--- app/www/index.php
+++ app/www/index.php
@@ -1,2 +1,12 @@
 <?
-	phpinfo();
+//phpinfo();
+
+$conn = pg_connect("host=postgres port=5432 dbname=postgres user=root password=root");
+
+if (!$conn) {
+    echo "Произошла ошибка при соединении с базой!";
+    echo pg_last_error($conn);
+    exit;
+}
+
+echo "Ok!";
